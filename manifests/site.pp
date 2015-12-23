b4687c4d52790bc0a6fd9d473c1c80f039d7afaa
Justin Bass <justinalanbass@gmail.com>
2015-12-23 20:37:53 +0000

    Using tmux-source module and creates to clean up code and ensure it runs only once

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,4 +1,4 @@
-include wget
+include tmux
 
 Exec {
     path =>  '/usr/bin:/usr/sbin:/bin:/usr/local/bin:/opt/local/bin:/usr/sfw/bin',
@@ -6,18 +6,19 @@ Exec {
 }
 
 node default {
-    package { 'git':
+    #Install and configure vim
+    package { 'vim':
         ensure => present,
     }
 
-    #Install vim
-    package { 'vim':
+    package { 'git':
         ensure => present,
     }
 
     exec {'get_linux_config':
         cwd => '/home/pi',
         command => 'git clone https://github.com/justinbass/linux_config.git',
+        creates => '/home/pi/linux_config',
         require => [ Package['git'], Package['vim'] ],
     }
 
@@ -33,44 +34,4 @@ node default {
         require => Exec['get_linux_config'],
     }
 
-    #Install tmux 2.0
-    wget::fetch { 'get_tmux2_tarball':
-        source      => 'http://downloads.sourceforge.net/project/tmux/tmux/tmux-2.0/tmux-2.0.tar.gz',
-        destination => '/tmp/tmux-2.0.tar.gz',
-        timeout     => 0,
-        verbose     => false,
-    }
-
-    file { '/tmp/tmux-2.0.tar.gz':
-        ensure => file,
-        mode => '0755',
-        source => '/tmp/tmux-2.0.tar.gz',
-    }
-
-    exec { 'untar_tmux2':
-        cwd => '/tmp',
-        command => 'tar xfz tmux-2.0.tar.gz',
-        require => File['/tmp/tmux-2.0.tar.gz'],
-    }
-
-    package { 'libevent-dev':
-        ensure => present,
-    }
-
-    package { 'libncurses5-dev':
-        ensure => present,
-    }
-
-    exec { 'build_tmux2':
-        cwd => '/tmp/tmux-2.0',
-        command => 'ls -l configure && ./configure && make && make install',
-        require => [ Exec['untar_tmux2'], Package['libevent-dev'], Package['libncurses5-dev'] ],
-        onlyif => 'if [ "`tmux -V`" = "tmux 2.0" ]; then echo 0; else echo 1; fi'
-    }
-
-    exec { 'clean_tmux':
-        cwd => '/tmp',
-        command => 'rm tmux-2.0.tar.gz',
-        require => Exec['build_tmux2'],
-    }
 }
